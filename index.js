a9ff4f51cd00eee065c98e049c5b02b55c3e9235
davidlucid <davidlucidsl@gmail.com>
2021-08-12 10:47:50 -0500

    Fixes + new collateral strategies

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ var fuseSafeLiquidator = new web3.eth.Contract(fuseSafeLiquidatorAbi, process.en
 const provider = new ethers.providers.JsonRpcProvider({ url: process.env.WEB3_HTTP_PROVIDER_URL });
 const authSigner = new ethers.Wallet(process.env.ETHEREUM_ADMIN_PRIVATE_KEY);
 
+const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+
 const UNISWAP_V2_PROTOCOLS = {
     "Uniswap": {
         router: "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
@@ -41,7 +43,11 @@ const COLLATERAL_REDEMPTION_STRATEGIES = {
     WSTEth: "0xca844845a3578296b3fcfe50fc3a1064a2922fbc",
     SOhm: "0xeBC0752232697F17EbfAA1f26aB8543EcEC35AE3",
     UniswapV3: "0x5E829D997294F7f1d40a45C0f6431aF13a381E63",
-    SushiBar: "0x5F2dF200636e203863819CbEaA02017CFabEc4D6"
+    SushiBar: "0x5F2dF200636e203863819CbEaA02017CFabEc4D6",
+    UniswapLpToken: "0x3659a0a9128ee84f143bdc83c4f3932cd8f552e7",
+    BadgerSett: "0x37c909fb7c6ad03d968898a2105da767ac736c80",
+    Harvest: "0xbb77A6a11A5998A6C7B9337f97Fd82f0D90f873b",
+    StakedSdt: "0x015e435df0bfb249990be78ce050bf8b3b88f757"
 };
 
 async function approveTokensToSafeLiquidator(erc20Address, amount) {
@@ -128,7 +134,7 @@ async function sendTransactionToSafeLiquidator(method, params, value, gasLimit, 
         const blockNumber = await web3.eth.getBlockNumber();
         var simulation = await flashbotsProvider.simulate(signedBundle, blockNumber + 1 );
         if (process.env.NODE_ENV !== "production") console.log("Simulated bundle for flashbots:", simulation);
-        if (simulation.results[0].error !== undefined || simulation.firstRevert !== undefined) throw "Error simulating flashbots-enabled " + method, "transaction: " + error;
+        if (simulation.results[0].error !== undefined || simulation.firstRevert !== undefined) throw "Error simulating flashbots-enabled " + method, "transaction: " + (simulation.results[0].error || simulation.firstRevert);
 
         // Send bundle
         for (var i = blockNumber + 1; i < blockNumber + 12; i++) {
@@ -351,26 +357,28 @@ async function getPotentialLiquidation(borrower, closeFactor, liquidationIncenti
 }
 
 async function getUniswapV2RouterByPreference(token) {
-    // Return SushiSwap router if converting from or to RGT, YAM, ALCX, or yveCRV-DAO
+    // Return SushiSwap router if converting from or to RGT, YAM, ALCX, yveCRV-DAO, OT-aUSDC-29DEC2022, OT-cDAI-29DEC2022
     return token.toLowerCase() == "0xd291e7a03283640fdc51b121ac401383a46cc623".toLowerCase() ||
         token.toLowerCase() == "0x0AaCfbeC6a24756c20D41914F2caba817C0d8521".toLowerCase() ||
         token.toLowerCase() == "0xdbdb4d16eda451d0503b854cf79d55697f90c8df".toLowerCase() ||
-        token.toLowerCase() == "0xc5bddf9843308380375a611c18b50fb9341f502a".toLowerCase() ?
+        token.toLowerCase() == "0xc5bddf9843308380375a611c18b50fb9341f502a".toLowerCase() ||
+        token.toLowerCase() == "0x8fcb1783bf4b71a51f702af0c266729c4592204a".toLowerCase() ||
+        token.toLowerCase() == "0x3d4e7f52efafb9e0c70179b688fc3965a75bcfea".toLowerCase() ?
         "0xd9e1ce17f2641f24ae83637ab66a2cca9c378b9f" : "0x7a250d5630b4cf539739df2c5dacb4c659f2488d";
 }
 
 async function getUniswapV2RouterByBestWethLiquidity(token) {
     // Get best Uniswap market for this token
-    var bestUniswapV2RouterForToken, bestUniswapLiquidityForToken = Fuse.Web3.utils.toBN(0);
+    var bestUniswapV2RouterForToken, bestUniswapLiquidityForToken = Web3.utils.toBN(0);
     var uniswapV2FactoryAbi = [{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}];
     var uniswapV2PairAbi = [{"constant":true,"inputs":[],"name":"getReserves","outputs":[{"internalType":"uint112","name":"_reserve0","type":"uint112"},{"internalType":"uint112","name":"_reserve1","type":"uint112"},{"internalType":"uint32","name":"_blockTimestampLast","type":"uint32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"}];
     for (const uniswapV2 of Object.values(UNISWAP_V2_PROTOCOLS)) {
-        var uniswapV2Factory = new fuse.web3.eth.Contract(uniswapV2FactoryAbi, uniswapV2.factory);
-        var uniswapV2Pair = await uniswapV2Factory.methods.getPair(token, Fuse.WETH_ADDRESS).call();
+        var uniswapV2Factory = new web3.eth.Contract(uniswapV2FactoryAbi, uniswapV2.factory);
+        var uniswapV2Pair = await uniswapV2Factory.methods.getPair(token, WETH_ADDRESS).call();
         if (uniswapV2Pair == "0x0000000000000000000000000000000000000000") continue;
-        uniswapV2Pair = new fuse.web3.eth.Contract(uniswapV2PairAbi, uniswapV2Pair);
+        uniswapV2Pair = new web3.eth.Contract(uniswapV2PairAbi, uniswapV2Pair);
         var reserves = await uniswapV2Pair.methods.getReserves().call();
-        var wethLiquidity = Fuse.Web3.utils.toBN(reserves[(await uniswapV2Pair.methods.token0().call()).toLowerCase() == Fuse.WETH_ADDRESS.toLowerCase() ? "0" : "1"])
+        var wethLiquidity = Web3.utils.toBN(reserves[(await uniswapV2Pair.methods.token0().call()).toLowerCase() == WETH_ADDRESS.toLowerCase() ? "0" : "1"])
         if (wethLiquidity.gt(bestUniswapLiquidityForToken)) {
             bestUniswapV2RouterForToken = uniswapV2.router;
             bestUniswapLiquidityForToken = wethLiquidity;
@@ -380,6 +388,7 @@ async function getUniswapV2RouterByBestWethLiquidity(token) {
 }
 
 async function getCollateralStrategies(token) {
+    var strategies = [];
     if (token.toLowerCase() == "0xcee60cfa923170e4f8204ae08b4fa6a3f5656f3a".toLowerCase()) strategies = ["CurveLpToken"]; // linkCRV
     if (token.toLowerCase() == "0xfd4d8a17df4c27c1dd245d153ccf4499e806c87d".toLowerCase()) strategies = ["CurveLiquidityGaugeV2"]; // linkCRV-gauge
     if (token.toLowerCase() == "0xf2db9a7c0ACd427A680D640F02d90f6186E71725".toLowerCase()) strategies = ["YearnYVaultV2", "CurveLpToken"]; // yvCurve-LINK
@@ -390,34 +399,43 @@ async function getCollateralStrategies(token) {
     if (token.toLowerCase() == "0x5f18c75abdae578b483e5f43f12a39cf75b973a9".toLowerCase()) strategies = ["YearnYVaultV2"]; // yvUSDC
     if (token.toLowerCase() == "0xcb550a6d4c8e3517a939bc79d0c7093eb7cf56b5".toLowerCase()) strategies = ["YearnYVaultV2"]; // yvWBTC
     if (token.toLowerCase() == "0x9d409a0a012cfba9b15f6d4b36ac57a46966ab9a".toLowerCase()) strategies = ["YearnYVaultV2"]; // yvBOOST
-    if (token.toLowerCase() == "0x31932e6e45012476ba3a3a4953cba62aee77fbbe".toLowerCase()) strategies = ["SOhm", "UniswapV2Liquidator"]; // sOHM
+    if (token.toLowerCase() == "0x04f2694c8fcee23e8fd0dfea1d4f5bb8c352111f".toLowerCase()) strategies = ["SOhm", "UniswapV2"]; // sOHM
     if (token.toLowerCase() == "0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B".toLowerCase()) strategies = ["UniswapV2"]; // TRIBE
     if (token.toLowerCase() == "0x23b608675a2b2fb1890d3abbd85c5775c51691d5".toLowerCase()) strategies = ["UniswapV1"]; // SOCKS
     if (token.toLowerCase() == "0x7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0".toLowerCase()) strategies = ["WSTEth", "CurveSwap"]; // wstETH
     if (token.toLowerCase() == "0xc53342fd7575f572b0ff4569e31941a5b821ac76".toLowerCase() || token.toLowerCase() == "0x3a707d56d538e85b783e8ce12b346e7fb6511f90".toLowerCase() || token.toLowerCase() == "0x51b0bcbeff204b39ce792d1e16767fe6f7631970".toLowerCase() || token.toLowerCase() == "0x2590f1fd14ef8bb0a46c7a889c4cbc146510f9c3".toLowerCase()) strategies = ["UniswapV3"]; // ETHV, iETHV, BTCV, iBTCV
-    return getCollateralStrategiesData(token, []);
+    if (token.toLowerCase() == "0x4d5ef58aac27d99935e5b6b4a6778ff292059991".toLowerCase() || token.toLowerCase() == "0x0F82E57804D0B1F6FAb2370A43dcFAd3c7cB239c".toLowerCase()) strategies = ["UniswapLpToken"]; // DPI-ETH, YAM-ETH
+    if (token.toLowerCase() == "0x62b9c7356a2dc64a1969e19c23e4f579f9810aa7".toLowerCase()) strategies = ["UniswapV2"]; // cvxCRV
+    if (token.toLowerCase() == "0x19d97d8fa813ee2f51ad4b4e04ea08baf4dffc28".toLowerCase()) strategies = ["BadgerSett"]; // bBADGER
+    if (token.toLowerCase() == "0x7e7e112a68d8d2e221e11047a72ffc1065c38e1a".toLowerCase()) strategies = ["BadgerSett", "UniswapV2"]; // bDIGG
+    if (token.toLowerCase() == "0xc4e15973e6ff2a35cc804c2cf9d2a1b817a8b40f".toLowerCase()) strategies = ["UniswapV2"]; // ibBTC
+    if (token.toLowerCase() == "0x1571eD0bed4D987fe2b498DdBaE7DFA19519F651".toLowerCase()) strategies = ["Harvest"]; // iFARM
+    if (token.toLowerCase() == "0xac14864ce5a98af3248ffbf549441b04421247d3".toLowerCase()) strategies = ["StakedSdt"]; // xSDT
+    return await getCollateralStrategiesData(token, strategies);
 }
 
 async function getCollateralStrategiesData(token, strategies) {
     var datas = [];
     var lastUniswapV2Router = undefined; // Ignore UniswapV2Routers of the first strategies if we have multiple; we only care about the last one
+    var lastToken = token; // Keep track of last token to be redeemed in chain of collateral redemption strategies
 
     for (var i = 0; i < strategies.length; i++) {
-        var [data, uniswapV2Router] = getCollateralStrategyData(token, strategies[i]);
+        var [data, uniswapV2Router, outputToken] = await getCollateralStrategyData(lastToken, strategies[i]);
+        if (outputToken) lastToken = outputToken;
         datas[i] = data;
         strategies[i] = COLLATERAL_REDEMPTION_STRATEGIES[strategies[i]];
         if (uniswapV2Router) lastUniswapV2Router = uniswapV2Router;
     }
 
     if (!lastUniswapV2Router) lastUniswapV2Router = await getUniswapV2RouterByPreference(token);
-    return [lastUniswapV2Router, datas, strategies];
+    return [lastUniswapV2Router, strategies, datas];
 }
 
 async function getCollateralStrategyData(token, strategy) {
     if (strategy == "CurveLiquidityGaugeV2") {
         // Get coins underlying LP token underling gauge
         var gaugeAbi = [{"name":"lp_token","outputs":[{"type":"address","name":""}],"inputs":[],"stateMutability":"view","type":"function","gas":1871}];
-        var gauge = new fuse.web3.eth.Contract(gaugeAbi, token);
+        var gauge = new web3.eth.Contract(gaugeAbi, token);
         token = await gauge.methods.lp_token().call();
         strategy = "CurveLpToken";
     }
@@ -425,7 +443,7 @@ async function getCollateralStrategyData(token, strategy) {
     if (strategy == "CurveLpToken") {
         // Get Curve pool coins
         var registryAbi = [{"name":"get_coins","outputs":[{"type":"address[8]","name":""}],"inputs":[{"type":"address","name":"_pool"}],"stateMutability":"view","type":"function","gas":12285},{"name":"get_pool_from_lp_token","outputs":[{"type":"address","name":""}],"inputs":[{"type":"address","name":"arg0"}],"stateMutability":"view","type":"function","gas":2446}];
-        var registry = new fuse.web3.eth.Contract(registryAbi, "0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c");
+        var registry = new web3.eth.Contract(registryAbi, "0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c");
         var pool = await registry.methods.get_pool_from_lp_token(token).call();
         var coins = await registry.methods.get_coins(pool).call();
     
@@ -437,7 +455,7 @@ async function getCollateralStrategyData(token, strategy) {
             if (coins[i] == "0x0000000000000000000000000000000000000000") break;
     
             // Break if coin is WETH
-            if (coins[i].toLowerCase() == Fuse.WETH_ADDRESS.toLowerCase()) {
+            if (coins[i].toLowerCase() == WETH_ADDRESS.toLowerCase()) {
                 bestUniswapV2Router = UNISWAP_V2_PROTOCOLS.Uniswap.router;
                 bestCurveCoinIndex = i;
                 bestUnderlying = coins[i];
@@ -457,23 +475,43 @@ async function getCollateralStrategyData(token, strategy) {
         }
     
         // Return strategy data and Uniswap V2 router
-        return [fuse.web3.eth.abi.encodeParameters(['uint8', 'address'], [bestCurveCoinIndex, bestUnderlying]), bestUniswapV2Router];
+        return [web3.eth.abi.encodeParameters(['uint8', 'address'], [bestCurveCoinIndex, bestUnderlying]), bestUniswapV2Router, bestUnderlying];
     }
     
-    if (strategy == "UniswapLiquidator") {
-      if (token.toLowerCase() == "0x383518188c0c6d7730d91b2c03a03c837814a899".toLowerCase()) return [fuse.web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.SushiSwap.router, ["0x383518188c0c6d7730d91b2c03a03c837814a899", "0x6b175474e89094c44da98b954eedeac495271d0f"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router]; // OHM => DAI
-      if (token.toLowerCase() == "0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B".toLowerCase()) return [fuse.web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.Uniswap.router, ["0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B", "0x956F47F50A910163D8BF957Cf5846D573E7f87CA"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router]; // TRIBE => FEI
+    if (strategy == "UniswapV2") {
+        if (token.toLowerCase() == "0x383518188c0c6d7730d91b2c03a03c837814a899".toLowerCase()) return [web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.SushiSwap.router, ["0x383518188c0c6d7730d91b2c03a03c837814a899", "0x6b175474e89094c44da98b954eedeac495271d0f"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router, "0x6b175474e89094c44da98b954eedeac495271d0f"]; // OHM => DAI
+        if (token.toLowerCase() == "0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B".toLowerCase()) return [web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.Uniswap.router, ["0xc7283b66Eb1EB5FB86327f08e1B5816b0720212B", "0x956F47F50A910163D8BF957Cf5846D573E7f87CA"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router, "0x956F47F50A910163D8BF957Cf5846D573E7f87CA"]; // TRIBE => FEI
+        if (token.toLowerCase() == "0x62b9c7356a2dc64a1969e19c23e4f579f9810aa7".toLowerCase()) return [web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.SushiSwap.router, ["0x62b9c7356a2dc64a1969e19c23e4f579f9810aa7", "0xD533a949740bb3306d119CC777fa900bA034cd52"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router, "0xD533a949740bb3306d119CC777fa900bA034cd52"]; // cvxCRV => CRV
+        if (token.toLowerCase() == "0x798D1bE841a82a273720CE31c822C61a67a601C3".toLowerCase()) return [web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.SushiSwap.router, ["0x798D1bE841a82a273720CE31c822C61a67a601C3", "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router, "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"]; // DIGG => WBTC
+        if (token.toLowerCase() == "0xc4e15973e6ff2a35cc804c2cf9d2a1b817a8b40f".toLowerCase()) return [web3.eth.abi.encodeParameters(['address', 'address[]'], [UNISWAP_V2_PROTOCOLS.SushiSwap.router, ["0xc4e15973e6ff2a35cc804c2cf9d2a1b817a8b40f", "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"]]), UNISWAP_V2_PROTOCOLS.Uniswap.router, "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599"]; // ibBTC => WBTC
     }
 
     if (strategy == "CurveSwap") {
-      // Get Curve pool for token => WETH
-      var registryAbi = [{"name":"find_pool_for_coins","outputs":[{"type":"address","name":""}],"inputs":[{"type":"address","name":"_from"},{"type":"address","name":"_to"}],"stateMutability":"view","type":"function"},{"name":"get_coin_indices","outputs":[{"type":"int128","name":""},{"type":"int128","name":""},{"type":"bool","name":""}],"inputs":[{"type":"address","name":"_pool"},{"type":"address","name":"_from"},{"type":"address","name":"_to"}],"stateMutability":"view","type":"function","gas":27456}];
-      var registry = new fuse.web3.eth.Contract(registryAbi, "0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c");
-      var pool = await registry.methods.find_pool_for_coins(token, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").call();
-      var indices = await registry.methods.get_coin_indices(pool, token, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").call();
+        // Get Curve pool for token => WETH
+        var registryAbi = [{"name":"find_pool_for_coins","outputs":[{"type":"address","name":""}],"inputs":[{"type":"address","name":"_from"},{"type":"address","name":"_to"}],"stateMutability":"view","type":"function"},{"name":"get_coin_indices","outputs":[{"type":"int128","name":""},{"type":"int128","name":""},{"type":"bool","name":""}],"inputs":[{"type":"address","name":"_pool"},{"type":"address","name":"_from"},{"type":"address","name":"_to"}],"stateMutability":"view","type":"function","gas":27456}];
+        var registry = new web3.eth.Contract(registryAbi, "0x7D86446dDb609eD0F5f8684AcF30380a356b2B4c");
+        var pool = await registry.methods.find_pool_for_coins(token, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").call();
+        var indices = await registry.methods.get_coin_indices(pool, token, "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE").call();
 
-      // Return strategy data and Uniswap V2 router
-      return [fuse.web3.eth.abi.encodeParameters(['address', 'int128', 'int128', 'address'], [pool, indices["0"], indices["1"], "0x0000000000000000000000000000000000000000"]), UNISWAP_V2_PROTOCOLS.Uniswap.router];
+        // Return strategy data and Uniswap V2 router
+        return [web3.eth.abi.encodeParameters(['address', 'int128', 'int128', 'address'], [pool, indices["0"], indices["1"], "0x0000000000000000000000000000000000000000"]), UNISWAP_V2_PROTOCOLS.Uniswap.router, WETH_ADDRESS];
+    }
+    
+    if (strategy == "YearnYVaultV2") {
+        var yVaultV2Abi = [{"name":"token","outputs":[{"type":"address","name":""}],"inputs":[],"stateMutability":"view","type":"function","gas":2831}];
+        var yVaultV2 = new web3.eth.Contract(yVaultV2Abi, token);
+        var outputToken = await yVaultV2.methods.token().call();
+        return ["0x0", undefined, outputToken];
+    }
+
+    if (strategy == "SOhm") return ["0x0", undefined, "0x383518188c0c6d7730d91b2c03a03c837814a899"];
+    if (strategy == "WSTEth") return ["0x0", undefined, "0xae7ab96520de3a18e5e111b5eaab095312d7fe84"];
+ 
+    if (strategy == "BadgerSett") {
+        var settAbi = [{"name":"token","outputs":[{"type":"address","name":""}],"inputs":[],"stateMutability":"view","type":"function","gas":2831}];
+        var sett = new web3.eth.Contract(settAbi, token);
+        var outputToken = await sett.methods.token().call();
+        return ["0x0", undefined, outputToken];
     }
 
     return ["0x0", undefined];
